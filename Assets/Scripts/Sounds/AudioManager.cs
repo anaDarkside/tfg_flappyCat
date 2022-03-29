b59d3afea6b329a700e15ce2a36fdb57ab3de58d
Ana <darkside@darkside.es>
2022-03-29 20:09:09 +0200

    Modify Music Volume
    added code to modify the music volume

--- Assets/Scripts/Sounds/AudioManager.cs
+++ Assets/Scripts/Sounds/AudioManager.cs
@@ -5,9 +5,10 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioMixer music;
+    public AudioMixer musicMixer;
     public AudioSource backgroundMusic;
     public static AudioManager instance;
+    public float masterVol;
 
     private void Awake() {
         if (instance == null) {
@@ -18,15 +19,19 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayAudio(backgroundMusic);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MasterVolume();
     }
 
+    public void MasterVolume(){
+        musicMixer.SetFloat("MasterVolume", masterVol);
+    }
+    
     public void PlayAudio(AudioSource audio) {
         audio.Play();
     }
