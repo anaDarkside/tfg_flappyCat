400125ff38693a932bb7b2216f43aca68cc75734
Ana <darkside@darkside.es>
2022-04-02 01:37:09 +0200

    Music between scenes
    Music between scenes

--- Assets/Scripts/Sounds/AudioManager.cs
+++ Assets/Scripts/Sounds/AudioManager.cs
@@ -12,28 +12,37 @@ public class AudioManager : MonoBehaviour
     [Range (-80,0)] public float masterVol;
     public Slider volumeSlider;
 
+    private static AudioMixer staticMusicMixer;
+    private static AudioSource staticBackgroundMusic;
+
+    public void PlayAudio(AudioSource audio) 
+    {
+        //if (audio.isPlaying) return;
+        audio.Play();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        PlayAudio(backgroundMusic);
-        volumeSlider.value = (float)Math.Pow(10.0f, masterVol/20.0f);
-        //volumeSlider.value = (masterVol + 80)/90.0f; //TODO: cambiar formula
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       // MasterVolume(); //TODO: llamar desde un onChange()
+        if (staticMusicMixer == null)
+        {
+            staticMusicMixer = musicMixer;
+            staticBackgroundMusic = backgroundMusic;
+            PlayAudio(staticBackgroundMusic);
+            volumeSlider.value = (float)Math.Pow(10.0f, masterVol/20.0f);
+            DontDestroyOnLoad(this.gameObject);
+        } else 
+        {
+            staticMusicMixer.GetFloat("MasterVolume", out masterVol);
+            volumeSlider.value = (float)Math.Pow(10.0f, masterVol/20.0f);
+        }
     }
 
     public void MasterVolume()
     {
-        musicMixer.SetFloat("MasterVolume", (float)Math.Log10(volumeSlider.value)*20.0f); 
-        //musicMixer.SetFloat("MasterVolume", -80+volumeSlider.value*90); //TO DO: buscar formula correcta
+        staticMusicMixer.SetFloat("MasterVolume", (float)Math.Log10(volumeSlider.value)*20.0f);
+        staticMusicMixer.GetFloat("MasterVolume", out masterVol);
+        volumeSlider.value = (float)Math.Pow(10.0f, masterVol/20.0f);
     }
-    
-    public void PlayAudio(AudioSource audio) 
-    {
-        audio.Play();
-    }
+
 }
