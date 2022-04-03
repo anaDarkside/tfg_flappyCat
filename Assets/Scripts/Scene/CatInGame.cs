006b73d4ecc3d3a8faa948126ee7d534e7044ec0
Ana <darkside@darkside.es>
2022-04-03 20:12:19 +0200

    Modify designs of several windows
    Modify designs of several windows

--- Assets/Scripts/Scene/CatInGame.cs
+++ Assets/Scripts/Scene/CatInGame.cs
@@ -9,6 +9,12 @@ public class CatInGame : MonoBehaviour
     public new CameraMovement camera;
     public float jumpAmount;
     private float positionInCamera;
+    private bool preparingJump;
+    private bool jumping;
+    private bool falling;
+    private float startJump;
+
+    public float secondsPrepareJump;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +23,9 @@ public class CatInGame : MonoBehaviour
           rigidbody = GetComponent<Rigidbody2D>();
           anim.SetBool("isWalking", true);
           anim.SetBool ("isJumping", false);
+          preparingJump = false;
+          jumping = false;
+          falling = false;
 
           positionInCamera = transform.position.x-camera.transform.position.x;
     }
@@ -24,10 +33,36 @@ public class CatInGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0)){
-            rigidbody.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+        if (Input.GetMouseButton(0) && !preparingJump && !jumping && !falling){
+            preparingJump = true;
+            startJump = Time.time;
+            anim.SetBool("isJumping", true);
+            anim.SetBool("isWalking", false);
+        }
+
+        if(jumping && rigidbody.velocity.y < 0)
+        {
+            jumping = false;
+            falling = true;
+        }
+
+        if(falling && rigidbody.velocity.y == 0)
+        {
+            falling = false;
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isWalking", true);
         }
         
         transform.position = new Vector3(positionInCamera + camera.transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    void FixedUpdate()
+    {
+        if(preparingJump && (Time.time - startJump) > secondsPrepareJump)
+        {
+            rigidbody.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            preparingJump = false;
+            jumping = true;
+        }
     }
 }
